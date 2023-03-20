365b3d10450a41c41015e0ee5162871fd491cfad
Nihal Uzunyayla <99769537+nihalUzunyayla@users.noreply.github.com>
2023-03-20 16:46:47 +0300

    Modifications for HW-2
    Performing matrix-vector multiplication.

--- CENG342_HW1/19050111018.c
+++ CENG342_HW1/19050111018.c
@@ -1,8 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 #include <hellomake.h>
 
-int main() {
-  // call a function in another file
-  myPrintHelloMake();
+void multiplyMatrixAndVector(double**, double*, double*, int, int);
 
-  return(0);
+int main(int argc, char *argv[]) {
+
+    int rows = atoi(argv[1]);
+    int columns = atoi(argv[2]);
+    char *fileName = argv[3];
+
+    srand48(19050111018);
+
+    double **randomMatrix = (double **)malloc(rows * sizeof(double *));
+    for (int i = 0; i < rows; i++) {
+        randomMatrix[i] = (double *)malloc(columns * sizeof(double));
+    }
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < columns; j++) {
+            randomMatrix[i][j] = 1.0 + (double)rand() / RAND_MAX * 99.0;
+        }
+    }
+
+    double *randomVector = (double *)malloc(columns * sizeof(double));
+    double *result = (double *)malloc(rows * sizeof(double));
+
+    for (int i = 0; i < columns; i++) {
+        randomVector[i] = 1.0 + (double)rand() / RAND_MAX * 50.0;
+    }
+
+    multiplyMatrixAndVector(randomMatrix, randomVector, result, rows, columns);
+
+
+    FILE *file = fopen(fileName, "w");
+    if (file == NULL) {
+        printf("Error: cannot open output file.\n");
+        return 1;
+    }
+
+    fprintf(file, "%s\n", "Result:\n");
+    for (int i = 0; i < rows; i++) {
+        fprintf(file, "%.2lf\n", result[i]);
+    }
+    fclose(file);
+    printf("File created successfully.\n");
+
+    for (int i = 0; i < rows; i++) {
+        free(randomMatrix[i]);
+    }
+    free(randomMatrix);
+    free(randomVector);
+    free(result);
+
+    // call a function in another file
+    myPrintHelloMake();
+
+    return 0;
 }
+
+void multiplyMatrixAndVector(double **matrix, double *vector, double *result, int rows, int columns) {
+    for (int i = 0; i < rows; i++) {
+        double sum = 0.0;
+        for (int j = 0; j < columns; j++) {
+            sum += matrix[i][j] * vector[j];
+        }
+        result[i] = sum;
+    }
+}
+
