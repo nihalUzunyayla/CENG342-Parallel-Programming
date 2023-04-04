8c58abec2a5ac0107bf6db4a9d135c9cc1f9de30
Nihal Uzunyayla <99769537+nihalUzunyayla@users.noreply.github.com>
2023-04-04 19:15:09 +0300

    Adding Make to HW3(New version)

--- CENG342_HW1/19050111018.c
+++ CENG342_HW1/19050111018.c
@@ -1,4 +1,3 @@
-
 //Nihal UZUNYAYLA 19050111018
 
 #include <stdio.h>
@@ -6,56 +5,112 @@
 #include <string.h>
 #include <time.h>
 #include <hellomake.h>
+#include <mpi.h>
 
-void multiplyMatrixAndVector(double**, double*, double*, int, int);
+#define MASTER_RANK 0
+
+void matrixGeneration(int, int, double**);
+void vectorGeneration(int, double*);
+void multiplyMatrixAndVector(double**, double*, double*, int, int, int, int);
 
 int main(int argc, char *argv[]) {
 
+    int pRank, pCount;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &pCount);
+    MPI_Comm_rank(MPI_COMM_WORLD, &pRank);
+      
+    if (argc != 4) {
+        if (pRank == MASTER_RANK) {
+            printf("Usage: mpirun -n <num_processes> ./%s <rows> <columns> <output_file>\n", argv[0]);
+        }
+        MPI_Finalize();
+        return 1;
+    }
+
     int rows = atoi(argv[1]);
-    int columns = atoi(argv[2]);
+    int cols = atoi(argv[2]);
     char *fileName = argv[3];
 
-    srand48(19050111018);
+    double **randomMatrix;
+    double *randomVector;
+    double *finResult;
 
-    double **randomMatrix = (double **)malloc(rows * sizeof(double *));
+    randomMatrix = (double **)malloc(rows * sizeof(double *));
     for (int i = 0; i < rows; i++) {
-        randomMatrix[i] = (double *)malloc(columns * sizeof(double));
+        randomMatrix[i] = (double *)malloc(cols * sizeof(double));
     }
+    matrixGeneration(rows, cols, randomMatrix);
 
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < columns; j++) {
-            randomMatrix[i][j] = 1.0 + (double)rand() / RAND_MAX * 99.0;
-        }
-    }
+    randomVector = (double *)malloc(cols * sizeof(double));
+    vectorGeneration(cols, randomVector);
 
-    double *randomVector = (double *)malloc(columns * sizeof(double));
-    double *result = (double *)malloc(rows * sizeof(double));
-
-    for (int i = 0; i < columns; i++) {
-        randomVector[i] = 1.0 + (double)rand() / RAND_MAX * 50.0;
-    }
-
-    multiplyMatrixAndVector(randomMatrix, randomVector, result, rows, columns);
-
-    // call a function in another file
-    fileWritingMake(fileName, result, rows);
+    finResult = (double *)malloc(rows * sizeof(double));
+    multiplyMatrixAndVector(randomMatrix, randomVector, finResult, rows, cols, pRank, pCount);
     
-    for (int i = 0; i < rows; i++) {
-        free(randomMatrix[i]);
+    if (pRank == MASTER_RANK) {
+        fileWritingMake(fileName, finResult, rows);
     }
-    free(randomMatrix);
-    free(randomVector);
-    free(result);
+
+    MPI_Finalize();
+
+    
 
     return 0;
+
 }
 
-void multiplyMatrixAndVector(double **matrix, double *vector, double *result, int rows, int columns) {
+void matrixGeneration(int rows, int cols, double **matrix)
+{
+    srand48(19050111018);
+
     for (int i = 0; i < rows; i++) {
-        double sum = 0.0;
-        for (int j = 0; j < columns; j++) {
-            sum += matrix[i][j] * vector[j];
+        for (int j = 0; j < cols; j++) {
+            matrix[i][j] = 1.0 + (double)rand() / RAND_MAX * 99.0;
         }
-        result[i] = sum;
     }
 }
+
+void vectorGeneration(int cols, double *vector)
+{
+    srand48(19050111018);
+
+    for (int i = 0; i < cols; i++) {
+        vector[i] = 1.0 + (double)rand() / RAND_MAX * 99.0;
+    }
+
+}
+
+void multiplyMatrixAndVector(double **matrix, double *vector, double *finResult, int rows, int cols, int pRank, int pCount) 
+{
+    double *locResult = (double *)malloc(rows * sizeof(double));
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    double start_time = MPI_Wtime();
+
+    for (int i = pRank; i < rows; i += pCount) {
+        double sum = 0.0;
+        for (int j = 0; j < cols; j++) {
+            sum += matrix[i][j] * vector[j];
+        }
+        locResult[i] = sum;
+    }
+
+    if (pRank == MASTER_RANK) {
+        
+        for(int i = 0; i < rows; i++){
+            finResult[i] = 0;
+        }
+    }
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Reduce(locResult, finResult, rows, MPI_DOUBLE, MPI_SUM, MASTER_RANK, MPI_COMM_WORLD);
+    
+    if (pRank == MASTER_RANK) {
+        double end_time = MPI_Wtime();
+        printf("\nElapsed time is %f seconds for parallel mxv with %d processes\n", end_time - start_time, pCount);
+    }
+    
+}
+
+
